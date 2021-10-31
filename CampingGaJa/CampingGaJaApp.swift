5fc0df565d8c0f8907fb52ec9aa9c7a155cc6c17
underTheBlueSun <wind7129@naver.com>
2021-10-31 11:03:56 +0900

    Initial Commit

---
+++ CampingGaJa/CampingGaJaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampingGaJaApp.swift
+//  CampingGaJa
+//
+//  Created by underTheBlueSun on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct CampingGaJaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
