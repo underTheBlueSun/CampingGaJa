7d0d00944389e5fc715459413fa598b791313144
underTheBlueSun <wind7129@naver.com>
2021-10-31 13:42:38 +0900

    ..

--- CampingGaJa/CampingGaJaApp.swift
+++ CampingGaJa/CampingGaJaApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CampingGaJaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            SearchDefaultRealm()
         }
     }
 }
